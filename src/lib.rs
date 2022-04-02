d38b5a444d3302325a53f45c5c528b27b57f1181
YOSHIOKA Takuma <lo48576@hard-wi.red>
2022-04-02 14:55:25 +0900

    Add basic files

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! Tree data structure.
+#![forbid(unsafe_code)]
+//#![forbid(unsafe_op_in_unsafe_fn)]
+//#![forbid(clippy::undocumented_unsafe_blocks)]
+#![warn(rust_2018_idioms)]
+// `clippy::missing_docs_in_private_items` implies `missing_docs`.
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(clippy::must_use_candidate)]
+#![warn(clippy::unwrap_used)]
