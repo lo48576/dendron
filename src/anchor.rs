d0403a307d5d9a200f6a2dfbbf2dd9e4d5fc9ac6
YOSHIOKA Takuma <lo48576@hard-wi.red>
2022-04-13 06:44:23 +0900

    Add `InsertAs` anchor type

--- src/anchor.rs
+++ src/anchor.rs
@@ -12,3 +12,19 @@ pub enum AdoptAs {
     /// As the next sibling.
     NextSibling,
 }
+
+/// Target destination to insert, append, or prepend a node.
+#[derive(Clone)]
+// All variants have the common suffix "Of", but this is intended.
+// Variants would be used as, for example, `InsertAs::FirsChildOf(some_node)`.
+#[allow(clippy::enum_variant_names)]
+pub enum InsertAs<T> {
+    /// As the first child.
+    FirstChildOf(T),
+    /// As the last child.
+    LastChildOf(T),
+    /// As the previous sibling.
+    PreviousSiblingOf(T),
+    /// As the next sibling.
+    NextSiblingOf(T),
+}
