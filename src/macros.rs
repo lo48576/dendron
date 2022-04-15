da42235ed556e035069fae3ba130cb8bf1fe50d3
YOSHIOKA Takuma <lo48576@hard-wi.red>
2022-04-16 06:09:16 +0900

    Simplify tree creation macro syntax
    Make descendants omissible when a node has none.

--- src/macros.rs
+++ src/macros.rs
@@ -2,24 +2,42 @@
 
 /// A macro that evaluates to the root node of a new tree.
 ///
+/// Syntax is simple:
+///
+/// * For non-root nodes:
+///     + `data` to describe a leaf node.
+///     + `(data, [descendants_recursive])` to describe a node with descendants.
+/// * For the root node:
+///     + `data` to describe single node tree.
+///     + `data, [descendants_recursive]` to describe a tree with descendants.
+///
 /// # Examples
 ///
 /// ```
 /// use dendron::tree_node;
 /// use dendron::traverse::DftEvent::{Close, Open};
 ///
+/// //  root
+/// //  |-- 0
+/// //  |   |-- 0-0
+/// //  |   `-- 0-1
+/// //  |-- 1
+/// //  `-- 2
+/// //      |-- 2-0
+/// //      |   `-- 2-0-0
+/// //      `-- 2-1
 /// let root = tree_node! {
 ///     "root", [
-///         ("0", [
-///             ("0-0", []),
-///             ("0-1", []),
+///         /("0", [
+///             "0-0",
+///             "0-1",
 ///         ]),
-///         ("1", []),
-///         ("2", [
-///             ("2-0", [
-///                 ("2-0-0", []),
+///         "1",
+///         /("2", [
+///             /("2-0", [
+///                 "2-0-0",
 ///             ]),
-///             ("2-1", []),
+///             "2-1",
 ///         ]),
 ///     ]
 /// };
@@ -52,21 +70,49 @@
 /// ```
 #[macro_export]
 macro_rules! tree_node {
+    // A tree should have a root node.
     () => {
         compile_error!("tree should have a root node")
     };
+    // A tree without descendants.
     ($data:expr) => {
         $crate::Node::new_tree($data)
     };
-    ($data:expr, [$($children:tt)*]) => {{
+    // A tree (possibly) with descendants.
+    ($data:expr, [$($descendant:tt)*]) => {{
         let root = $crate::HotNode::new_tree($data);
-        $crate::tree_node!(@@append_child, root, [$($children)*]);
+        $crate::tree_node!(@@process_descendant, root, [$($descendant)*]);
         root.plain()
     }};
-    (@@append_child, $parent:ident, [$(($child_data:expr, [$($descendants:tt)*])),* $(,)?]) => {{
-        $({
-            let node = $parent.create_as_last_child($child_data);
-            $crate::tree_node!(@@append_child, node, [$($descendants)*]);
-        })*
-    }};
+    // The last non-leaf node.
+    (@@process_descendant, $parent:ident, [
+        /($data:expr, [$($descendant:tt)*]) $(,)?
+    ]) => {
+        {
+            let node = $parent.create_as_last_child($data);
+            $crate::tree_node!(@@process_descendant, node, [$($descendant)*]);
+        }
+    };
+    // A non-last non-leaf node.
+    (@@process_descendant, $parent:ident, [
+        /($data:expr, [$($descendant:tt)*]),
+        $($rest:tt)*
+    ]) => {
+        {
+            let node = $parent.create_as_last_child($data);
+            $crate::tree_node!(@@process_descendant, node, [$($descendant)*]);
+        }
+        $crate::tree_node!(@@process_descendant, $parent, [$($rest)*]);
+    };
+    // The last leaf node.
+    (@@process_descendant, $parent:ident, [$data:expr $(,)?]) => {
+        $parent.create_as_last_child($data);
+    };
+    // A non-last non-leaf node.
+    (@@process_descendant, $parent:ident, [$data:expr, $($rest:tt)*]) => {
+        $parent.create_as_last_child($data);
+        $crate::tree_node!(@@process_descendant, $parent, [$($rest)*]);
+    };
+    // No children.
+    (@@process_descendant, $parent:ident, []) => {};
 }
